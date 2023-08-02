1292728581df22d71f2cf3eb161fc39ae23ea54f
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-01 23:46:28 -0300

    Visitor - Prática

---
+++ src/behavioural/visitor/alcoholic-drink.ts
@@ -0,0 +1,12 @@
+import { TaxVisitorProtocol } from "./interfaces/tax-visitor-protocol";
+import { VisitableProduct } from "./visitable-product";
+
+export class AlcoholicDrink extends VisitableProduct {
+    constructor(protected price: number) {
+        super('Food', price);
+    }
+
+    getPriceWithTaxes(visitor: TaxVisitorProtocol): number {
+        return visitor.calculateTaxesForAlcoholicDrink(this)
+    }
+}
---
+++ src/behavioural/visitor/main.ts
@@ -0,0 +1,20 @@
+import { Food } from "./food";
+import { Cigarette } from './cigarette';
+import { AlcoholicDrink } from "./alcoholic-drink";
+import { BrazilTaxVisitor } from './brazil-tax-visitor';
+import { UsTaxVisitor } from './us-tax-visitor';
+
+const food = new Food(10);
+const cigarette = new Cigarette(5);
+const alcoholicDrink = new AlcoholicDrink(5);
+const brazilTaxVisitor = new BrazilTaxVisitor();
+const usTaxVisitor = new UsTaxVisitor();
+
+const cart = [food, cigarette, alcoholicDrink];
+const total = cart.reduce((sum, item) => item.getPrice() + sum, 0);
+const totalWithTaxesBrazil = cart.reduce((sum, item) => item.getPriceWithTaxes(brazilTaxVisitor) + sum, 0);
+const totalWithTaxesUs = cart.reduce((sum, item) => item.getPriceWithTaxes(usTaxVisitor) + sum, 0);
+
+console.log(total);
+console.log(totalWithTaxesBrazil);
+console.log(totalWithTaxesUs);
